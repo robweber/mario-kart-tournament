82f4c0a7c0ee029754d816481ab4b32b95d9e177
robweber <robweberjr@gmail.com>
2017-11-26 11:27:04 -0600

    Choose the active game

--- app/Controller/AdministrationController.php
+++ app/Controller/AdministrationController.php
@@ -11,10 +11,25 @@ App::uses('AppController', 'Controller');
  * @link https://book.cakephp.org/2.0/en/controllers/pages-controller.html
  */
 class AdministrationController extends AppController {
-	public $uses = array();
+	public $uses = array('Game','Setting');
 
+	public function beforeRender(){
+		$settings = $this->Setting->find('list',array('fields'=>array('Setting.name','Setting.value')));
+		$this->set('settings',$settings);
+	}
 
 	public function index() {
 		$this->set('title_for_layout','Administration');
+		
+		if($this->request->is('post'))
+		{
+			$this->Session->setFlash('Active Game Updated');
+			
+			//update the active game id
+			$this->Setting->query('update settings set value = ' . $this->data['ActiveGame']['ActiveGame']  . ' where name = "active_game"');
+		}
+		
+		$games = $this->Game->find('list',array('fields'=>array('Game.id','Game.name')));
+		$this->set('games',$games);
 	}
 }
--- app/View/Administration/index.ctp
+++ app/View/Administration/index.ctp
@@ -1,3 +1,7 @@
+<?php echo $this->Form->create('ActiveGame') ?>
+<?php echo $this->Form->input('ActiveGame',array('type'=>'select','options'=>$games,'selected'=>$settings['active_game'])); ?>
+<?php echo $this->Form->submit('Update'); ?>
 <ul>
-	<li>Start Tournament</li>
+	<li>Users</li>
 </ul>
+
